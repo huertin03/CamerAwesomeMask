ca29ab9e883019953ad99cabfd02506e9c79e999
Álvaro Huertas <huertin03@gmail.com>
2024-01-30 09:44:27 +0100

    Changed size to see if mask resizes

--- lib/src/orchestrator/models/masks/awesome_mask.dart
+++ lib/src/orchestrator/models/masks/awesome_mask.dart
@@ -28,7 +28,8 @@ class AwesomeMask {
   static AwesomeMask get BourdeauxBottle => AwesomeMask(
         name: 'Bordeaux',
         outputFilter: CustomPaint(
-          size: Size(437.65, (437.65 * 3.806167400881057).toDouble()),
+          // size: Size(437.65, (437.65 * 3.806167400881057).toDouble()),
+          size: Size(1000, (1000 * 3.806167400881057).toDouble()),
           painter: Bourdeaux(),
           foregroundPainter: Bourdeaux(),
         ),
